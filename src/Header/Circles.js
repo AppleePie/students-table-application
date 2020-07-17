8db093e7b0b33e36695c38ba16cccaad1e99b771
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-07-17 13:51:16 +0500

    Correct tome mistakes #2

--- src/Header/Circles.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import circles from './IMG/group.svg';
-
-export default function VectorGroup(props) {
-    return (
-        <img className="circles" src={circles} alt = "Vector"/>
-    );
-}
--- src/Header/index.js
+++ src/Header/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import './index.css';
 
@@ -6,16 +5,6 @@ function Header() {
     return (
         <div className="header">
             <img className="circles" src='/IMG/circles.svg' alt = "circles.svg"/>
-=======
-import './index.css';
-import Circles from './Circles';
-import React from 'react';
-
-function Header(props) {
-    return (
-        <div className="header">
-            <Circles/>
->>>>>>> 18540fd6ec77811be180384da7d284210d310cf9
             <label className="text-students-header">STUDENTS</label>
         </div>
     );
